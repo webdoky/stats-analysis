04dc81956d4042fa49f99cf7ab6090b2916cd173
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2024-03-04 23:08:21 +0200

    Label updates

---
+++ label-updates/label_update.py
@@ -0,0 +1,30 @@
+from github import PullRequest
+
+from get_popularity import get_popularity
+
+LABELS = ['Criticality: zero', 'Criticality: low', 'Criticality: mild', 'Criticality: high']
+
+def get_label(popularity: float) -> str:
+    if popularity < 1.0:
+        return LABELS[0]
+    elif popularity < 10.0:
+        return LABELS[1]
+    elif popularity < 100.0:
+        return LABELS[2]
+    else:
+        return LABELS[3]
+
+def label_update(update_pull_request: PullRequest):
+    print(f"Labeling {update_pull_request.title}")
+    labels = update_pull_request.get_labels()
+    slug = update_pull_request.title.split(": ")[1]
+    popularity = get_popularity(slug)
+    label_to_add = get_label(popularity)
+    labels_to_remove = [label.name for label in labels if label.name.startswith("Criticality:") and label.name != label_to_add]
+    if label_to_add not in labels:
+        print(f"Adding {label_to_add} to {update_pull_request.title}")
+        update_pull_request.add_to_labels(label_to_add)
+    for label in labels_to_remove:
+        print(f"Removing {label} from {update_pull_request.title}")
+        update_pull_request.remove_from_labels(label)
+    
---
+++ label-updates/run.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import os
+
+import dotenv
+from github import Auth, Github
+
+from label_update import label_update
+
+dotenv.load_dotenv()
+if os.getenv('CONTENT_GITHUB_TOKEN') is None:
+    raise Exception('No CONTENT_GITHUB_TOKEN environment variable found')
+
+auth = Auth.Token(os.getenv('CONTENT_GITHUB_TOKEN'))
+
+with Github(auth=auth) as gh:
+    repo = gh.get_repo('webdoky/content')
+    update_pull_requests = (pull for pull in repo.get_pulls(state='open', base='master') if pull.title.startswith("update"))
+    for update_pull_request in update_pull_requests:
+        label_update(update_pull_request)
+    
