80e4e7ef771583529bdde5b2190437f6a3fa60b1
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2024-03-04 22:18:23 +0200

    Refactor into few jobs

--- edit-issue/run.py
+++ edit-issue/run.py
@@ -6,22 +6,22 @@ import dotenv
 from github import Auth, Github
 
 dotenv.load_dotenv()
-if os.getenv('GITHUB_TOKEN') is None:
-    raise Exception('No GITHUB_TOKEN environment variable found')
+if os.getenv('STATS_GITHUB_TOKEN') is None:
+    raise Exception('No STATS_GITHUB_TOKEN environment variable found')
 
-auth = Auth.Token(os.getenv('GITHUB_TOKEN'))
+auth = Auth.Token(os.getenv('STATS_GITHUB_TOKEN'))
 
-DISCUSSION_ID = 1
+ISSUE_ID = 1
 table_header = "| URL | Clicks |\n| --- | --- |"
 
 def get_markdown(weights):
     print(weights[0])
-    return table_header + "\n" + "\n".join((f"| https://webdoky.org/uk/docs/{weight['URL']} | {weight['Clicks']} |" for weight in weights if weight['Clicks'] > 0.0))
+    return table_header + "\n" + "\n".join((f"| https://developer.mozilla.org/en-us/docs/{weight['URL']} | {weight['Clicks']} |" for weight in weights if weight['Clicks'] > 0.0))
 
 weights_data = open('./_Prediction.json').read()
 weights = json.loads(weights_data)
 
 with Github(auth=auth) as gh:
     repo = gh.get_repo('webdoky/stats-analysis')
-    discussion = repo.get_issue(DISCUSSION_ID)
-    discussion.edit(body=get_markdown(weights))
+    issue = repo.get_issue(ISSUE_ID)
+    issue.edit(body=get_markdown(weights))
